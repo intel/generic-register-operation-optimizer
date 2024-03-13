e32d5b529a4e874152ea8448659e23160a893f38
Ben Deane <ben.deane@intel.com>
2024-03-13 15:35:27 -0600

    update to use CICD infra

--- test/mmio_bus_pbt.py
+++ test/mmio_bus_pbt.py
@@ -1,26 +1,15 @@
-from hypothesis import given, settings, strategies as st
-import cppyy
-
-cppyy.add_include_path("include")
-cppyy.add_include_path("test")
-cppyy.add_include_path("build/_deps/cpp-baremetal-concurrency-src/include")
-cppyy.add_include_path("build/_deps/cpp-std-extensions-src/include")
-cppyy.add_include_path("build/_deps/cpp-baremetal-senders-and-receivers-src/include")
-cppyy.add_include_path("build/_deps/mp11-src/include")
-
-cppyy.include("mmio_bus_pbt.hpp")
-
-from cppyy.gbl import test
+from hypothesis import given, strategies as st
 
 def uints(size):
     return st.integers(min_value=0, max_value=2**size-1)
 
-def do_mmio_bus_write(type, initial, mask, id_mask, id_value, write_value):
+def do_mmio_bus_write(test, type, initial, mask, id_mask, id_value, write_value):
     write_value = mask & write_value
     id_mask = ~mask & id_mask
     id_value = id_mask & id_value
     initial = (~id_mask & initial) | id_value
 
+    from cppyy.gbl import test
     test.reg64 = initial
 
     test.bus_write[type, mask, id_mask, id_value](test.addr64, write_value)
@@ -31,23 +20,19 @@ def do_mmio_bus_write(type, initial, mask, id_mask, id_value, write_value):
     keep_mask = ~mask & ~id_mask
     assert ((keep_mask & test.reg64) == (keep_mask & initial))
 
-@settings(max_examples=1000)
 @given(uints(64), uints(64), uints(64), uints(64), uints(64))
-def test_mmio_bus_writes_with_id_mask_64(initial, mask, id_mask, id_value, write_value):
-    do_mmio_bus_write("std::uint64_t", initial, mask, id_mask, id_value, write_value)
+def test_mmio_bus_writes_with_id_mask_64(cppyy_test, initial, mask, id_mask, id_value, write_value):
+    do_mmio_bus_write(cppyy_test, "std::uint64_t", initial, mask, id_mask, id_value, write_value)
 
-@settings(max_examples=1000)
 @given(uints(32), uints(32), uints(32), uints(32), uints(32))
-def test_mmio_bus_writes_with_id_mask_32(initial, mask, id_mask, id_value, write_value):
-    do_mmio_bus_write("std::uint32_t", initial, mask, id_mask, id_value, write_value)
+def test_mmio_bus_writes_with_id_mask_32(cppyy_test, initial, mask, id_mask, id_value, write_value):
+    do_mmio_bus_write(cppyy_test, "std::uint32_t", initial, mask, id_mask, id_value, write_value)
 
-@settings(max_examples=1000)
 @given(uints(16), uints(16), uints(16), uints(16), uints(16))
-def test_mmio_bus_writes_with_id_mask_16(initial, mask, id_mask, id_value, write_value):
-    do_mmio_bus_write("std::uint16_t", initial, mask, id_mask, id_value, write_value)
+def test_mmio_bus_writes_with_id_mask_16(cppyy_test, initial, mask, id_mask, id_value, write_value):
+    do_mmio_bus_write(cppyy_test, "std::uint16_t", initial, mask, id_mask, id_value, write_value)
 
-@settings(max_examples=1000)
 @given(uints(8), uints(8), uints(8), uints(8), uints(8))
-def test_mmio_bus_writes_with_id_mask_8(initial, mask, id_mask, id_value, write_value):
-    do_mmio_bus_write("std::uint8_t", initial, mask, id_mask, id_value, write_value)
+def test_mmio_bus_writes_with_id_mask_8(cppyy_test, initial, mask, id_mask, id_value, write_value):
+    do_mmio_bus_write(cppyy_test, "std::uint8_t", initial, mask, id_mask, id_value, write_value)
 
