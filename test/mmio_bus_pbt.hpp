225714d50e9ae2743ce6964cbfab1eb9ba918aae
Ben Deane <ben.deane@intel.com>
2024-03-14 07:59:54 -0600

    CI updates for python tests

--- test/mmio_bus_pbt.hpp
+++ test/mmio_bus_pbt.hpp
@@ -6,15 +6,15 @@
 #include <async/then.hpp>
 
 #include <groov/mmio_bus.hpp>
-#include <groov/path.hpp>       
+#include <groov/path.hpp>
 
 namespace test {
 uint64_t volatile reg64{};
 auto addr64 = reinterpret_cast<std::uintptr_t>(&reg64);
-            
-template<typename T, T mask, T id_mask, T id_value>
+
+template <typename T, T mask, T id_mask, T id_value>
 void bus_write(std::uintptr_t addr64, T write_value) {
-    groov::mmio_bus<>::write<mask, id_mask, id_value>(addr64, write_value) | 
-        async::sync_wait();                     
+    groov::mmio_bus<>::write<mask, id_mask, id_value>(addr64, write_value) |
+        async::sync_wait();
 }
-} // namespace test     
+} // namespace test
--- test/mmio_bus_pbt.py
+++ test/mmio_bus_pbt.py
@@ -1,4 +1,4 @@
-from hypothesis import given, strategies as st
+from hypothesis import given, settings, strategies as st
 
 def uints(size):
     return st.integers(min_value=0, max_value=2**size-1)
@@ -20,18 +20,22 @@ def do_mmio_bus_write(test, type, initial, mask, id_mask, id_value, write_value)
     keep_mask = ~mask & ~id_mask
     assert ((keep_mask & test.reg64) == (keep_mask & initial))
 
+@settings(deadline=None)
 @given(uints(64), uints(64), uints(64), uints(64), uints(64))
 def test_mmio_bus_writes_with_id_mask_64(cppyy_test, initial, mask, id_mask, id_value, write_value):
     do_mmio_bus_write(cppyy_test, "std::uint64_t", initial, mask, id_mask, id_value, write_value)
 
+@settings(deadline=None)
 @given(uints(32), uints(32), uints(32), uints(32), uints(32))
 def test_mmio_bus_writes_with_id_mask_32(cppyy_test, initial, mask, id_mask, id_value, write_value):
     do_mmio_bus_write(cppyy_test, "std::uint32_t", initial, mask, id_mask, id_value, write_value)
 
+@settings(deadline=None)
 @given(uints(16), uints(16), uints(16), uints(16), uints(16))
 def test_mmio_bus_writes_with_id_mask_16(cppyy_test, initial, mask, id_mask, id_value, write_value):
     do_mmio_bus_write(cppyy_test, "std::uint16_t", initial, mask, id_mask, id_value, write_value)
 
+@settings(deadline=None)
 @given(uints(8), uints(8), uints(8), uints(8), uints(8))
 def test_mmio_bus_writes_with_id_mask_8(cppyy_test, initial, mask, id_mask, id_value, write_value):
     do_mmio_bus_write(cppyy_test, "std::uint8_t", initial, mask, id_mask, id_value, write_value)
