74c4bef73cdb58c7d11157d3403bfb60d5f932e4
Luke Valenty <luke.valenty@intel.com>
2024-12-10 14:29:42 -0800

    Add omnibus header groov/groov.hpp

---
+++ include/groov/groov.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <groov/config.hpp>
+#include <groov/mmio_bus.hpp>
+#include <groov/path.hpp>
+#include <groov/read.hpp>
+#include <groov/read_spec.hpp>
+#include <groov/write.hpp>
+#include <groov/write_spec.hpp>
---
+++ usage_test/main.cpp
@@ -0,0 +1,47 @@
+#include <async/concepts.hpp>
+#include <async/just.hpp>
+#include <async/just_result_of.hpp>
+#include <async/sync_wait.hpp>
+
+#include <groov/groov.hpp>
+
+#include <cassert>
+#include <cstdint>
+
+namespace {
+struct bus {
+    template <auto Mask, auto IdMask, auto IdValue>
+    static auto write(auto addr, auto value) -> async::sender auto {
+        return async::just_result_of([=] { *addr = (*addr & ~Mask) | value; });
+    }
+
+    template <auto Mask> static auto read(auto addr) -> async::sender auto {
+        return async::just_result_of([=] { return *addr; });
+    }
+};
+
+using F0 = groov::field<"field0", std::uint8_t, 0, 0>;
+using F1 = groov::field<"field1", std::uint8_t, 4, 1>;
+using F2 = groov::field<"field2", std::uint8_t, 7, 5>;
+
+std::uint32_t data{};
+using R =
+    groov::reg<"reg", std::uint32_t, &data, groov::w::replace, F0, F1, F2>;
+
+using G = groov::group<"group", bus, R>;
+constexpr auto grp = G{};
+} // namespace
+
+auto main() -> int {
+    using namespace groov::literals;
+    data = 0xa5u;
+    async::just(grp / "reg"_r) //
+        | groov::read          //
+        | async::then([](auto spec) {
+              spec["reg"_r] ^= 0xff;
+              return spec;
+          })           //
+        | groov::write //
+        | async::sync_wait();
+    assert(data == 0x5au);
+}
